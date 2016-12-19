5f868609e4d6a7506a0813dffbd63790fb9fc705
Daniel Tsui <tsui.daniel@gmail.com>
2016-12-19 13:59:41 +1100

    add react & co

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import * as myModule from './myModule'
 import './index.css'
 
 console.log(myModule.intro)
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(
+  <App name="world" />, 
+  document.getElementById('root')
+);
